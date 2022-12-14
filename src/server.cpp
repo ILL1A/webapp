93fea8ffe240e03854734892e39a9c0496a7b126
ill1a <f11x99d@gmail.com>
2022-12-14 01:19:59 +0100

    implemented port validation using regular expressions

--- src/server.cpp
+++ src/server.cpp
@@ -1,18 +1,18 @@
 #include <iostream> 
 #include <string>
+#include <regex> 
 #include "include/logger.h"
 
 using namespace std;
 using namespace logger;
 
-int get_port(string str) {
-	if (str.size() > 6) return -1;
-	int port = 0;
-	for (int i = 0; i < str.size(); i ++) {
-		if (str[i] < '0' || str[i] > '9') return -1;
-		port = port * 10 + (str[i] - '0');
+int get_port(const char* str) {
+	regex port("^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$");
+	if (regex_match(str, port)) {
+		return atoi(str);
+	} else {
+		return -1;
 	}
-	return port;
 }
 
 int main(int argc, char* argv[]) {
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
 		error("you need to provide port");
 		return 0;
 	}
-	int port = get_port((string)argv[1]);
+	int port = get_port(argv[1]);
 	if (port == -1) {
 		error("incorrect port");
 		return 0;
